b01be0611557036bfe4ceaffa45e8ac9ebcf29d9
Suraj <38291736+suraj1102@users.noreply.github.com>
2024-02-16 02:14:11 +0530

    Read image file in main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -4,10 +4,14 @@
 using namespace cv;
 using namespace std;
 
+const string assetPath = "/Users/suraj/Documents/Coding/forUni/final/Path-Finder/assets/";
+
 int main(int argc, char** argv )
 {
+    // Load Image
+    const string filename = "10-10.png";
     Mat image;
-    image = imread("./assets/10-10.png");
+    image = imread(assetPath + filename);
     if ( !image.data )
     {
         printf("No image data \n");
@@ -15,16 +19,18 @@ int main(int argc, char** argv )
     }
     cout << image.size() << endl;
 
+    // Declare Height and Width of Image
     int height = image.size().height;
     int width = image.size().width;
 
+    // startPoint: (x1, y1) , goalPoint: (x2, y2)
     unsigned int x1 = 0;
     unsigned int y1 = 0;
     unsigned int x2 = 4;
     unsigned int y2 = 4;
 
     
-
+    // Display Image
     namedWindow("Display Image", WINDOW_AUTOSIZE);
     imshow("Display Image", image);
     
